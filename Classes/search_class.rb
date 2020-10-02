27854edf2093ae4920857bd6b20d50c6e6ff6d94
Tye-Hadfield <tye_hadfield@hotmail.com>
2020-10-03 00:46:54 +1000

    have been adding a output for the types of pokemon if chosen in the pokedex to be seen, I am working on how to not display a pokemons 2nd type if they do not have one.

--- Classes/search_class.rb
+++ Classes/search_class.rb
@@ -26,27 +26,73 @@ def pokemonTestPokeDexNumer
     user_answer = true
     
     while user_answer == true
-        
+        system "clear"
         puts "Choose number between 1-151"
         
         user_choice = gets.chomp.to_i
         
         select_pokemon = data.select {|hash| hash[:pokedexnumber] == user_choice }[0]
         
-    table = TTY::Table.new(["Pokedex Number","Name","type(s)","HP stats","Attack stats","Defense stats","SP_Atk stats","SP_Def stats","Speed stats","Generation"], [["#{select_pokemon[:pokedexnumber]}", "#{select_pokemon[:name]}", "#{select_pokemon[:type_1]} #{select_pokemon[:type_2]}","#{select_pokemon[:hp]}","#{select_pokemon[:attack]}","#{select_pokemon[:defense]}","#{select_pokemon[:spatk]}","#{select_pokemon[:spdef]}","#{select_pokemon[:speed]}","#{select_pokemon[:generation]}"]])
-    puts table.render(:ascii, alignment: [:center],padding: [0.5,0.5,0.5,0.5])
+        table = TTY::Table.new(["Pokedex Number","Name","type(s)","HP stats","Attack stats","Defense stats","SP_Atk stats","SP_Def stats","Speed stats","Generation"], [["#{select_pokemon[:pokedexnumber]}", "#{select_pokemon[:name]}", "#{select_pokemon[:type_1]} #{select_pokemon[:type_2]}","#{select_pokemon[:hp]}","#{select_pokemon[:attack]}","#{select_pokemon[:defense]}","#{select_pokemon[:spatk]}","#{select_pokemon[:spdef]}","#{select_pokemon[:speed]}","#{select_pokemon[:generation]}"]])
+        puts table.render(:ascii, alignment: [:center],padding: [0.5,0.5,0.5,0.5])
+        
+        type_of_pokemon = select_pokemon[:type_1]
+        type_of_pokemon2 = select_pokemon[:type_2]
+        
+        
+        
+        
+        def typechart(type_of_pokemon, type_of_pokemon2,types_data)
+                        
+            
+            pokemon_type1 = types_data.select {|hash| hash[:name] == type_of_pokemon }[0]
+            pokemon_type2 = types_data.select {|hash| hash[:name] == type_of_pokemon2 }[0]
+              
+            
+
+            puts "Type : #{pokemon_type1[:name]}"
+            puts "#{pokemon_type1[:name]} moves are super-effective against: #{pokemon_type1[:attack_super_effective]}"
+            puts "#{pokemon_type1[:name]} moves are not very effective against: #{pokemon_type1[:attack_not_super_effective]}"
+            puts "These types are not very effective against #{pokemon_type1[:name]} Pokémon: #{pokemon_type1[:defence_not_very_effective]}"
+            puts "These types are super-effective against #{pokemon_type1[:name]} Pokémon: #{pokemon_type1[:defence_super_very_effective]}"
+            puts "#{pokemon_type1[:name]} moves have no effect on: #{pokemon_type1[:attack_no_effect]}"
+            puts "These types have no effect on #{pokemon_type1[:name]} Pokémon: #{pokemon_type1[:defense_no_effect]}"
+           
+            
+                    
+            if test_type  == 6
+
+            puts "Type : #{pokemon_type2[:name]}"
+            puts "#{pokemon_type2[:name]} moves are super-effective against: #{pokemon_type2[:attack_super_effective]}"
+            puts "#{pokemon_type2[:name]} moves are not very effective against: #{pokemon_type2[:attack_not_super_effective]}"
+            puts "These types are not very effective against #{pokemon_type2[:name]} Pokémon: #{pokemon_type2[:defence_not_very_effective]}"
+            puts "These types are super-effective against #{pokemon_type2[:name]} Pokémon: #{pokemon_type2[:defence_super_very_effective]}"
+            puts "#{pokemon_type2[:name]} moves have no effect on: #{pokemon_type2[:attack_no_effect]}"
+            puts "These types have no effect on #{pokemon_type2[:name]} Pokémon: #{pokemon_type2[:defense_no_effect]}"
+            
+            elsif test_type == 0
+
+                
+
+            end
 
 
-    type_of_pokemon = select_pokemon[:type_1]
-    type_of_pokemon2 = select_pokemon[:type_2]
-    
-    pokemon_type1 = types_data.select {|hash| hash[:name] == type_of_pokemon }[0]
-    pokemon_type2 = types_data.select {|hash| hash[:name] == type_of_pokemon2 }[0]
 
+        end
+        
 
+        choices = {"Yes" => 1, "No" => 2}
+        
+        type_choices = prompt.select("Would you like to see the type chart for this Pokemon? (Yes or No)", choices)
 
-    choices = {"Yes" => 1, "No" => 2}
-
+        if type_choices == 1
+        
+         typechart(type_of_pokemon,type_of_pokemon2,types_data)
+ 
+        end
+        
+        choices = {"Yes" => 1, "No" => 2}
+        
     run_again = prompt.select("Would you like to search another Pokemon? (Yes or No)", choices)
     
     
@@ -75,7 +121,7 @@ def pokemonTestPokemonName
     user_answer = true
 
     while user_answer == true
-
+    system "clear"
     puts "What is the Name of the Pokemon you are searching for?"
 
     user_choice = gets.chomp.to_s.capitalize
