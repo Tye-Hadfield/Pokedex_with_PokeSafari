d13d0cc2b3fd71dee41031cbabb3d877767c82a0
Tye-Hadfield <tye_hadfield@hotmail.com>
2020-10-03 17:37:59 +1000

    remove gemfile.lock and will be pushing to Github to pull down on a fresh directory to see if this will work for new users.

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-bundle install
+echo "Do you wish to see the help instructions first? Type '1' for Yes or '2' for  No"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) make install;     
+                    ruby main.rb -h
+                            break;;
+        No ) break;;
+    esac
+done
 
-ruby main.rb
+
+echo "Do you wish to install this program? Type '1' for Yes or '2' for  No"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) make install;
+                    bundle install   
+                    ruby main.rb
+                            break;;
+        No ) exit;;
+    esac
+done
